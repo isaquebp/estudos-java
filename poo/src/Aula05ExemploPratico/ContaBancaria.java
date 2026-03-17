74a15b5187fdf50bc78e03fef9006f88cdb62b0f
isaquebp <isoq.dev@gmail.com>
2026-03-17 13:38:36 -0300

    aula5feita

--- poo/src/Aula05ExemploPratico/ContaBancaria.java
+++ poo/src/Aula05ExemploPratico/ContaBancaria.java
@@ -1,15 +1,27 @@
 package Aula05ExemploPratico;
-    // Atributos
+
+import java.util.Objects;
+
+// Atributos
 public class ContaBancaria {
     public int numConta;
-    protected char tipo;
+    protected String tipo;
     private String dono;
     private float saldo;
     private boolean status;
 
+    public void estadoAtual(){
+        System.out.println("--------------------------");
+        System.out.println("Conta: " + getNumConta());
+        System.out.println("Tipo: " + getTipo());
+        System.out.println("Dono: " + getDono());
+        System.out.println("Saldo: " + getSaldo());
+        System.out.println("Status: " + getStatus());
+    }
+
     public ContaBancaria(){
-        this.saldo = 0;
-        this.status = false;
+         setSaldo(0);
+        setStatus(false);
     }
 
     // Getters e Setters
@@ -21,10 +33,10 @@ public class ContaBancaria {
         this.numConta = nc;
     }
 
-    public char getTipo(){
+    public String getTipo(){
         return this.tipo;
     }
-    public void setTipo(char t){
+    public void setTipo(String t){
         this.tipo = t;
     }
 
@@ -50,20 +62,20 @@ public class ContaBancaria {
     }
 
     // Métodos
-    public void abrirConta(char t){
+    public void abrirConta(String t){
         setTipo(t);
         setStatus(true);
-        if (t == 'CC'){
+        if (Objects.equals(t, "CC")){
             setSaldo(50);
-        } else if (t == 'CP'){
+        } else if (Objects.equals(t, "CP")){
             saldo = 150;
         }
-
+        System.out.println("\nConta aberta com sucesso!");
     }
 
     public void fecharConta(){
         if  (getSaldo() > 0){
-            System.out.println("Conta com dinheiro");
+            System.out.println("Conta com dinheiro, impossível fechar");
         } else if (getSaldo() < 0){
             System.out.println("Conta em débito");
         } else {
@@ -72,39 +84,43 @@ public class ContaBancaria {
     }
 
     public void depositar(float v){
-        if (getStatus() == true){
+        if (getStatus()){
             setSaldo(getSaldo() + v);
+            System.out.println("Depósito realizado com sucesso na conta de " + getDono());
         } else {
             System.out.println("Impossível depositar!");
         }
     }
 
     public void sacar(float v){
-        if(getStatus() == true){
-            if (getSaldo() > 0){
+        if(getStatus()){
+            if (getSaldo() >= 0 && getSaldo() >= v){
                 setSaldo(getSaldo() - v);
-            } else {
-                System.out.println("Impossível sacar");
+                System.out.println("Saque realizado com sucesso na conta de " + getDono());
+            } else{
+                System.out.println("Saldo insuficiente para sacar");
             }
+        } else {
+            System.out.println("Impossível sacar de uma conta fechada");
         }
     }
 
     public void pagarMensal(float pagarM){
-        float v;
-        if (getTipo() == 'CC'){
+        float v = 0;
+        if (Objects.equals(getTipo(), "CC")){
             v = 12;
-        } else{
+        } else if (Objects.equals(getTipo(), "CP")){
             v = 20;
         }
-        if (getStatus() == true){
+        if (getStatus()){
             if (getSaldo() > v){
                 setSaldo(getSaldo() - v);
-                System.out.println("Mensalidade paga");
+                System.out.println("Mensalidade paga com sucesso por " + getDono());
             }else {
                 System.out.println("Saldo insuficiente");
             }
         } else {
-            System.out.println("Impossível pagar");
+            System.out.println("Impossível pagar uma conta fechada");
         }
     }
 }
--- poo/src/Aula05ExemploPratico/Main.java
+++ poo/src/Aula05ExemploPratico/Main.java
@@ -2,6 +2,26 @@ package Aula05ExemploPratico;
 
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria b1 = new ContaBancaria();
+        ContaBancaria p1 = new ContaBancaria();
+        p1.setNumConta(1);
+        p1.setDono("Marcos");
+        p1.abrirConta("CC");
+
+        ContaBancaria p2 = new ContaBancaria();
+        p2.setNumConta(2);
+        p2.setDono("Leide");
+        p2.abrirConta("CP");
+
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(1000);
+
+        p1.sacar(150);
+        p1.fecharConta();
+
+        p1.estadoAtual();
+        p2.estadoAtual();
+
+
     }
 }
